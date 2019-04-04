a2df6171d16a3691423d5151318fafda65e7675a
kishore2709 <kishorereddy.papatla@gmail.com>
2019-04-03 17:35:41 -0700

    First Commit

--- src/test/java/com/confidential/AdminPortal/AdminPortalApplicationTests.java
+++ src/test/java/com/confidential/AdminPortal/AdminPortalApplicationTests.java
@@ -2,11 +2,10 @@ package com.confidential.AdminPortal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+/*@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest*/
 public class AdminPortalApplicationTests {
 
 	@Test
