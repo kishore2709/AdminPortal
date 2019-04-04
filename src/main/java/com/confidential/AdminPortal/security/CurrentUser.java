40d0f1cdf031a57641ba9d87690985151f1a27db
kishore2709 <kishorereddy.papatla@gmail.com>
2019-04-03 17:13:05 -0700

    First Commit

---
+++ src/main/java/com/confidential/AdminPortal/security/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.confidential.AdminPortal.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
