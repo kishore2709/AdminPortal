8de6d255dee30e6e2043ab5c4b8521035020a12d
kishore2709 <kishorereddy.papatla@gmail.com>
2019-04-12 17:54:48 -0700

    Added Lambok

--- src/main/java/com/springapp/modules/security/JwtAuthenticationResponse.java
+++ src/main/java/com/springapp/modules/security/JwtAuthenticationResponse.java
@@ -1,26 +1,14 @@
 package com.springapp.modules.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class JwtAuthenticationResponse {
     private String accessToken;
     private String tokenType = "Bearer";
+    private final JwtUser user;
 
-    public JwtAuthenticationResponse(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
+ 
 }
--- src/main/java/com/springapp/modules/security/controller/AuthController.java
+++ src/main/java/com/springapp/modules/security/controller/AuthController.java
@@ -1,9 +1,16 @@
 package com.springapp.modules.security.controller;
 
+import java.net.URI;
+import java.util.Collections;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,8 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.confidential.AdminPortal.exception.AppException;
 import com.confidential.AdminPortal.payload.LoginRequest;
@@ -26,79 +31,88 @@ import com.springapp.modules.model.RoleName;
 import com.springapp.modules.model.User;
 import com.springapp.modules.security.JwtAuthenticationResponse;
 import com.springapp.modules.security.JwtTokenUtil;
+import com.springapp.modules.security.JwtUser;
 import com.springapp.modules.security.repository.RoleRepository;
 import com.springapp.modules.security.repository.UserRepository;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.Collections;
+import com.springapp.modules.security.service.MyUserDetailsService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController<Auth> {
 
-                Logger log = LoggerFactory.getLogger(AuthController.class);
+	Logger log = LoggerFactory.getLogger(AuthController.class);
 
-    @Autowired
-    AuthenticationManager authenticationManager;
+	@Autowired
+	AuthenticationManager authenticationManager;
 
-    @Autowired
-    UserRepository userRepository;
+	@Autowired
+	UserRepository userRepository;
 
-    @Autowired
-    RoleRepository roleRepository;
+	@Autowired
+	RoleRepository roleRepository;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
-    @Autowired
-    JwtTokenUtil tokenProvider;
-    
-                
-                 @PostMapping("/signin") public ResponseEntity<?>
-                authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-                                System.out.println("This is a DEBUG message."+passwordEncoder.encode(loginRequest.getPassword()));
-                                log.debug("This is a DEBUG message."+passwordEncoder.encode(loginRequest.getPassword()));
-                Authentication authentication = authenticationManager.authenticate( new
-                UsernamePasswordAuthenticationToken( loginRequest.getUsernameOrEmail(),
-                loginRequest.getPassword() ) );
-                
-                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                
-                 String jwt = tokenProvider.generateToken(authentication); return
-                ResponseEntity.ok(new JwtAuthenticationResponse(jwt)); 
-                 }
-                
-    @SuppressWarnings("unchecked")
-                @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
-        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return new ResponseEntity(new ApiResponse(false,  "Username is already taken!"),
-                    HttpStatus.BAD_REQUEST);
-        }
+	@Autowired
+	JwtTokenUtil tokenProvider;
+	
+	@Autowired
+    private MyUserDetailsService userDetailsService;
 
-        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"),
-                    HttpStatus.BAD_REQUEST);
-        }
 
-        // Creating user's account
-        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
-                signUpRequest.getEmail(), signUpRequest.getPassword());
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+		
+		  final JwtUser jwtUser = (JwtUser)
+		  userDetailsService.loadUserByUsername(loginRequest.getUsernameOrEmail());
+		  
+		  
+		  if(!jwtUser.getPassword().equals(passwordEncoder.encode(loginRequest.
+		  getPassword()))){ throw new AccountExpiredException("wrong password"); }
+		  
+		  if(!jwtUser.isEnabled()){ throw new
+		  AccountExpiredException("The account has been disabled, please contact the administrator"
+		  ); }
+		 
+	        
+		Authentication authentication = 
+				authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsernameOrEmail(), loginRequest.getPassword()));
 
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        Role userRole = roleRepository.findByName(RoleName.USER)
-                .orElseThrow(() -> new AppException("User Role not set."));
+		final String jwt = tokenProvider.generateToken(authentication);
+		return ResponseEntity.ok(new JwtAuthenticationResponse(jwt,jwtUser));
+// return ResponseEntity.ok(new AuthenticationInfo(token,jwtUser)); - sending user details in jwtUser
+	}
 
-        user.setRoles(Collections.singleton(userRole));
+	@SuppressWarnings("unchecked")
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return new ResponseEntity(new ApiResponse(false, "Username is already taken!"), HttpStatus.BAD_REQUEST);
+		}
 
-        User result = userRepository.save(user);
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"), HttpStatus.BAD_REQUEST);
+		}
 
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentContextPath().path("/api/users/{username}")
-                .buildAndExpand(result.getUsername()).toUri();
-System.out.println("User registered successfully");
-        return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
-    }
+		// Creating user's account
+		User user = new User(signUpRequest.getName(), signUpRequest.getUsername(), signUpRequest.getEmail(),
+				signUpRequest.getPassword());
+
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+		Role userRole = roleRepository.findByName(RoleName.USER)
+				.orElseThrow(() -> new AppException("User Role not set."));
+
+		user.setRoles(Collections.singleton(userRole));
+
+		User result = userRepository.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/{username}")
+				.buildAndExpand(result.getUsername()).toUri();
+		System.out.println("User registered successfully");
+		return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
+	}
 }
