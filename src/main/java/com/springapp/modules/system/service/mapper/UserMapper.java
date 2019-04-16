a87be2f6e1bffe0b68eff67e60fa4921f8b756ba
kishore2709 <kishorereddy.papatla@gmail.com>
2019-04-15 17:52:30 -0700

    updated pom.xml

--- src/main/java/com/springapp/modules/system/service/mapper/UserMapper.java
+++ src/main/java/com/springapp/modules/system/service/mapper/UserMapper.java
@@ -7,7 +7,8 @@ import com.springapp.mapper.EntityMapper;
 import com.springapp.modules.system.domain.User;
 import com.springapp.modules.system.service.dto.UserDTO;
 
-@Mapper(componentModel = "spring",uses = {RoleMapper.class, DeptMapper.class, JobMapper.class},unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring",uses = {RoleMapper.class, DeptMapper.class, JobMapper.class},
+unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper extends EntityMapper<UserDTO, User> {
 
 }
