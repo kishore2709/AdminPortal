4fd73094ad44822ebb878b708e47abafbb356682
kishore2709 <kishorereddy.papatla@gmail.com>
2019-04-15 17:11:54 -0700

    added mappers

--- src/main/java/com/springapp/modules/system/service/mapper/UserMapper.java
+++ src/main/java/com/springapp/modules/system/service/mapper/UserMapper.java
@@ -1,7 +1,5 @@
 package com.springapp.modules.system.service.mapper;
 
-import javax.swing.tree.RowMapper;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -9,7 +7,8 @@ import com.springapp.mapper.EntityMapper;
 import com.springapp.modules.system.domain.User;
 import com.springapp.modules.system.service.dto.UserDTO;
 
-@Mapper(componentModel = "spring",uses = {RowMapper.class, DeptMapper.class, JobMapper.class},unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring",uses = {RoleMapper.class, DeptMapper.class, JobMapper.class},unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper extends EntityMapper<UserDTO, User> {
 
 }
+
